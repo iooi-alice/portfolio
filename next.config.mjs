5eceebe3a3e22f53d27a217d489e5fcf02acb9e6
iooi-alice <kimhee21c@hanmail.net>
2024-09-08 17:40:05 +0900

    chore: next.config env, sass 옵션, cloudflare  외부  CDN 경로 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLOUDFLARE_URL: process.env.NEXT_PUBLIC_CLOUDFLARE_URL,
+  },
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    additionalData: `@import "@/styles/main.scss";`,
+  },
+  images: {
+    domains: ['imagedelivery.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/gHftqKLNBQ-NOWw8XS0zUw/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
