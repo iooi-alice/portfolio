6441b9908fb62d070982297d21f68e9c4788234f
iooi-alice <kimhee21c@hanmail.net>
2024-09-09 22:14:02 +0900

    feat: expertise 리스트 상수 선언

---
+++ src/constants/expertiseList.ts
@@ -0,0 +1,23 @@
+export const EXPERTISE_LIST = [
+  {
+    id: 0,
+    menu: 'UX Research',
+  },
+  {
+    id: 1,
+    menu: 'Product Design',
+  },
+
+  {
+    id: 2,
+    menu: 'Web Development',
+  },
+  {
+    id: 3,
+    menu: 'Design Systems',
+  },
+  {
+    id: 4,
+    menu: 'Motion Design',
+  },
+]
