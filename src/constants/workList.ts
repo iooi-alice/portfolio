f198482e8e283f28e1311b5bfddacbfb433d21ff
iooi-alice <kimhee21c@hanmail.net>
2024-09-09 22:13:41 +0900

    feat: work list 상수 선언

---
+++ src/constants/workList.ts
@@ -0,0 +1,22 @@
+import { IMAGES } from './images'
+
+export const WORK_LIST = [
+  {
+    id: 'work-01',
+    title: 'Integrated Network Management System',
+    desc: 'Creating customizable widgets that provide users with the information they need, while establishing visual hierarchy and depth between components through Glassmorphism.',
+    image: IMAGES.work_01,
+  },
+  {
+    id: 'work-02',
+    title: 'Integrated Network Management System',
+    desc: 'Wallboard-style monitoring dashboard that detects real-time issues, providing instant visibility into system performance and potential disruptions.',
+    image: IMAGES.work_02,
+  },
+  {
+    id: 'work-03',
+    title: 'InfoMap 2.0',
+    desc: 'Info Map is a real-time monitoring dashboard that detects issues and ensures smooth operations by helping quickly identify and resolve problems through a map displaying the regional distribution of incidents.',
+    image: IMAGES.work_03,
+  },
+]
