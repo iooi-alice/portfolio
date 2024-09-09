2a467d204420d64e755e06f2a590ceb23cf4776b
iooi-alice <kimhee21c@hanmail.net>
2024-09-09 22:15:19 +0900

    feat: about section 구현

---
+++ src/components/page/landing/AboutSection/index.tsx
@@ -0,0 +1,76 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+
+import classNames from 'classnames/bind'
+
+import { ABOUT_DESCRIPTIONS } from '@/constants/aboutDescriptionList'
+import { EXPERTISE_LIST } from '@/constants/expertiseList'
+import useScroller from '@/hooks/useScroller'
+
+import styles from './AboutSection.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface AboutSectionProps {
+  setTitle: Dispatch<SetStateAction<string>>
+}
+
+const AboutSection = ({ setTitle }: AboutSectionProps) => {
+  const trigger = useScroller(cx('is-motion'))
+  const [isActive, setIsActive] = useState(0)
+
+  const handleClickMenu = (activeId: number) => {
+    setIsActive(activeId)
+  }
+
+  useEffect(() => {
+    if (
+      trigger.current &&
+      trigger.current.classList.contains(cx('is-motion'))
+    ) {
+      setTitle('What I Do')
+    }
+  }, [trigger, setTitle])
+
+  return (
+    <section ref={trigger} className={cx('section-about')}>
+      <h2 className='visually-hidden'>About Me</h2>
+      <ul className={cx('expertise-list')}>
+        {EXPERTISE_LIST.map((item) => (
+          <li className={cx('expertise-item')} key={`expertise-key-${item.id}`}>
+            <button
+              className={cx('btn-menu', { 'is-active': isActive === item.id })}
+              type='button'
+              aria-label='btn-expertise-menu'
+              onClick={() => handleClickMenu(item.id)}
+            >
+              <span className={cx('btn-menu-text')} data-menu={item.menu}>
+                {item.menu}
+              </span>
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <ul className={cx('description-list')}>
+        {ABOUT_DESCRIPTIONS.map((desc) => (
+          <li
+            className={cx('description-item', {
+              'is-open': isActive === desc.id,
+            })}
+            key={`desc-key-${desc.id}`}
+          >
+            <div className={cx('description-card')}>
+              <h4 className={cx('description-title')}>{desc.title}</h4>
+              <div className={cx('description-textbox')}>
+                <p className={cx('description-text')}>{desc.desc_ko}</p>
+                <p className={cx('description-text')}>{desc.desc_en}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default AboutSection
