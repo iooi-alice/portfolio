72ae949f3afeb2757496445a21fb53f22cbb8305
iooi-alice <kimhee21c@hanmail.net>
2024-09-09 22:14:27 +0900

    feat: about description 리스트 상수 선언

---
+++ src/constants/aboutDescriptionList.ts
@@ -0,0 +1,42 @@
+export const ABOUT_DESCRIPTIONS = [
+  {
+    id: 0,
+    title: 'Empathize with Users',
+    desc_ko:
+      '사용자의 요구사항, 동기, 그리고 문제점을 깊이 이해하기 위해 페르소나와 여정 지도를 활용합니다. 이러한 UX 리서치 과정을 통해 얻은 인사이트를 바탕으로, 사용자 중심의 설계와 최적화된 경험을 제공하기 위한 방향을 설정하고 있습니다.',
+    desc_en:
+      'I utilize personas and journey maps to gain a deep understanding of users’ needs, motivations, and pain points. The insights gained from this UX research process guide me in setting the direction for user-centered design and delivering optimized experiences.',
+  },
+  {
+    id: 1,
+    title: 'Crafting Intuitive and Functional User Experiences',
+    desc_ko:
+      '리서치 단계에서 얻은 인사이트를 바탕으로, 사용자에게 직관적이고 편안한 경험을 제공하는 UI를 설계합니다. 인터페이스의 시각적 요소뿐만 아니라, 기능적으로도 원활한 경험을 제공하는 데 중점을 두고 작업합니다. 단순히 문제에 대한 정답을 주는 도구를 디자인하는 것이 아닌, 사용자와 소통하며 함께 문제를 해결할 수 있는 솔루션을 제시하는 것을 목표로 하고 있습니다.',
+    desc_en:
+      'Based on insights gained during the research phase, I design intuitive and user-friendly UIs. I focus not only on the visual elements of the interface but also on providing a seamless functional experience. My goal is to create solutions that foster communication and collaboration with users, rather than simply offering tools that provide predefined answers.',
+  },
+  {
+    id: 2,
+    title: 'From Design to Reliable Code',
+    desc_ko:
+      '디자인을 넘어, 기능적이고 신뢰성 있는 코드를 통해 직접 구현합니다. 이를 통해 협업 시 디자인과 개발 간의 원활한 커뮤니케이션을 이끌어냅니다. 함수형 컴포넌트에 대한 이해를 바탕으로, 재사용 가능한 컴포넌트 설계를 고려하여 구현합니다. 개발 과정에서의 문제 해결 경험을 블로그에 공유하며 지속적으로 자기계발에 힘쓰고 있습니다.',
+    desc_en:
+      'Beyond design, I also implement functional and reliable code. This helps facilitate smooth communication between design and development during collaboration. With a strong understanding of functional components, I focus on building reusable components. I also continuously improve myself by sharing problem-solving experiences from the development process on my blog.',
+  },
+  {
+    id: 3,
+    title: 'Building Consistent and Scalable Design Systems',
+    desc_ko:
+      '시스템의 일관성을 중요하게 생각하며, 확장성과 유지보수성을 고려해 모든 구성 요소가 재사용 가능하고 필요에 따라 유연하게 대응할 수 있도록 시스템을 구축합니다. 솔루션은 디자인 시스템을 기반으로 작업되며 이를 통해 모든 구성 요소가 통일된 규칙 하에 효율적으로 관리됩니다.',
+    desc_en:
+      'I prioritize consistency within the system and build it with scalability and maintainability in mind, ensuring that all components are reusable and adaptable when needed. The solution is developed based on a design system, allowing all components to be efficiently managed under unified guidelines.',
+  },
+  {
+    id: 4,
+    title: 'Enhancing User Experience with Interaction and Motion',
+    desc_ko:
+      '사용자와 제품 간의 원활한 소통을 위해 인터랙션과 모션 디자인을 활용하여 직관적이고 몰입감 있는 경험을 만드는 것을 중요하게 생각합니다. Keyframe과 transition을 사용해 인터랙션을 구현하여 모션 디자인에 대한 전체적인 방향을 공유하고 간단한 스크롤 트리거 이벤트는 GSAP을 활용하여 구현합니다.',
+    desc_en:
+      'I prioritize creating intuitive and immersive experiences by using interaction and motion design to facilitate smooth communication between users and the product. I implement interactions using keyframes and transitions to share the overall direction of the motion design, and I use GSAP to implement simple scroll-triggered events.',
+  },
+]
