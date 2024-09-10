8a04171c1fd9389ae19a091f594e5bcfecbb07e9
iooi-alice <kimhee21c@hanmail.net>
2024-09-10 14:15:37 +0900

    feat: 링크 이동 path 상수 선언

---
+++ src/constants/path.ts
@@ -0,0 +1,8 @@
+export const PATH = {
+  home_url: '/',
+  tickytocky_url: 'https://iooi-alice.github.io/ticky-tocky/',
+  ggf_url: 'https://good-game-friends.vercel.app/landing',
+  ggf_library_url: 'https://www.npmjs.com/package/ggf-ui',
+  mingle_url: 'https://mingle-zone.vercel.app/',
+  mingle_library_url: 'https://www.npmjs.com/package/mingle-ui',
+}
