803c54e30059087b6caaaa6b27659fb4112da481
iooi-alice <kimhee21c@hanmail.net>
2024-09-11 04:57:58 +0900

    feat: readme skill 리스트 상수 선언

---
+++ src/constants/readmeSkill.ts
@@ -0,0 +1,49 @@
+export const README_SKILL = [
+  {
+    id: 0,
+    skill: 'HTML',
+    content:
+      '시맨틱 태그와 메타 태그를 활용해 구조적이며 접근성이 높은 웹 페이지를 작성합니다.',
+  },
+  {
+    id: 1,
+    skill: 'CSS',
+    content:
+      'Flexbox와 Grid로 반응형 디자인을 구현하며, keyframes로 동적인 애니메이션을 제작합니다.',
+  },
+  {
+    id: 2,
+    skill: 'SCSS',
+    content:
+      'mixin, placeholder, nesting을 활용하여 모듈화된 유지보수 가능한 스타일을 작성합니다.',
+  },
+  {
+    id: 3,
+    skill: 'Tailwind',
+    content:
+      '유틸리티 클래스를 사용해 빠르고 일관성 있는 스타일링과 커스터마이징을 구현합니다.',
+  },
+  {
+    id: 4,
+    skill: 'Javascript',
+    content:
+      'DOM 조작, 이벤트 처리, 비동기 프로그래밍을 활용해 동적인 웹 애플리케이션을 개발합니다.',
+  },
+  {
+    id: 5,
+    skill: 'Typescript',
+    content:
+      '타입과 인터페이스를 정의하여 코드의 안정성을 높이며, 지속적으로 학습 중입니다.',
+  },
+  {
+    id: 6,
+    skill: 'React',
+    content:
+      '함수형 컴포넌트와 상태 관리를 이해하고, 재사용 가능한 컴포넌트를 설계 및 구현합니다.',
+  },
+  {
+    id: 7,
+    skill: 'Blog',
+    content: '개발 학습 과정과 인사이트를 블로그에 꾸준히 기록하고 공유합니다.',
+  },
+]
