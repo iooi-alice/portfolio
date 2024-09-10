d13e4ffdd7b45362d9c344bd6b3c89e5bfb28032
iooi-alice <kimhee21c@hanmail.net>
2024-09-11 05:23:09 +0900

    refactor: 수정된 contribution color 파일명으로 import문 변경

--- src/utils/getRandomColor.ts
+++ src/utils/getRandomColor.ts
@@ -1,4 +1,4 @@
-import { CONTRIBUTION_COLORS } from '@/constants/\bcontributionColors'
+import { CONTRIBUTION_COLORS } from '@/constants/contributionColors'
 
 export const getRandomColor = () => {
   const randomIndex = Math.floor(Math.random() * CONTRIBUTION_COLORS.length)
