6810c030d6aa437489b03f634a9f519b9267fe3c
iooi-alice <kimhee21c@hanmail.net>
2024-09-11 05:18:52 +0900

    refactor: hero 섹션 구조 개선 및 미디어쿼리 적용

--- src/components/page/landing/HeroSection/index.tsx
+++ src/components/page/landing/HeroSection/index.tsx
@@ -22,45 +22,53 @@ const HeroSection = ({ setTitle }: HeroSectionProps) => {
   return (
     <section ref={trigger} className={cx('section-hero')}>
       <h2 className='visually-hidden'>Portfolio Main View</h2>
-      <div className={cx('intro-left')}>
-        <div className={cx('intro-left-greeting')}>
-          <p>
-            <span>Hi ther</span>
-            <span className={cx('pacman')}>
-              <Image src={pacman.url} alt={pacman.alt} width={52} height={56} />
-            </span>
-          </p>
-        </div>
-        <div className={cx('intro-left-designer')}>
-          <p>
-            <span>I&#39;m</span>
-            <span>designer</span>
-          </p>
-        </div>
-        <div className={cx('intro-left-developer')}>
-          <p>
-            <span>&</span>
-            <span>web developer</span>
-          </p>
+      <div className={cx('intro-wrapper')}>
+        <div className={cx('intro-inner')}>
+          <div className={cx('intro-left')}>
+            <div className={cx('intro-left-greeting')}>
+              <p>
+                <span>Hi ther</span>
+                <span className={cx('pacman')}>
+                  <Image
+                    src={pacman.url}
+                    alt={pacman.alt}
+                    width={52}
+                    height={56}
+                  />
+                </span>
+              </p>
+            </div>
+            <div className={cx('intro-left-designer')}>
+              <p>
+                <span>I&#39;m</span>
+                <span>designer</span>
+              </p>
+            </div>
+            <div className={cx('intro-left-developer')}>
+              <p>
+                <span>&</span>
+                <span>web developer</span>
+              </p>
+            </div>
+          </div>
+          <div className={cx('intro-right')}>
+            <div className={cx('intro-right-ux')}>
+              <p>
+                <span>focused on</span>
+                <span>UX</span>
+              </p>
+            </div>
+            <div className={cx('intro-right-ds')}>
+              <p>
+                <span className={cx('plus')}>+</span>
+                <span>design systems</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className={cx('intro-right')}>
-        <div className={cx('intro-right-ux')}>
-          <p>
-            <span>focused on</span>
-            <span>UX</span>
-          </p>
-        </div>
-        <div className={cx('intro-right-ds')}>
-          <p>
-            <span className={cx('plus')}>+</span>
-            <span>design systems</span>
-          </p>
-        </div>
-      </div>
-
-      <div className={cx('view-footer')}>
+      <div className={cx('view-footer', 'sm-hidden')}>
         <span className={cx('view-footer-copyright')}>
           Soo Portfolio &#169; 2024
         </span>
