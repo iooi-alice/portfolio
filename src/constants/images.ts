4feb798e179e5b3292dc1bcab50440de7ebf7db0
iooi-alice <kimhee21c@hanmail.net>
2024-09-10 14:16:04 +0900

    refactor: spin 이미지, 프로젝트 배너 썸네일 이미지 상수 선언

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -22,12 +22,40 @@ export const IMAGES = {
     url: '/images/work-3.png',
     alt: 'work dashboard image',
   },
-  spin: {
-    url: '/images/spin.png',
+  work_spin: {
+    url: '/images/work-spin.png',
+    alt: 'work spin image',
+  },
+  project_spin: {
+    url: '/images/project-spin.png',
     alt: 'work spin image',
   },
   arrow_up: {
     url: '/images/arrow-up.png',
     alt: 'arrow up image',
+  },
+  ticky_tocky: {
+    url: '/images/project-thumbnail-1.png',
+    alt: 'project tickytocky thumbnail image',
+  },
+  apple_watch: {
+    url: '/images/project-thumbnail-2.png',
+    alt: 'project apple watch thumbnail image',
+  },
+  ggf_thumbnail: {
+    url: '/images/project-thumbnail-3.png',
+    alt: 'project ggf thumbnail image',
+  },
+  ggf_ui_thumbnail: {
+    url: '/images/project-thumbnail-4.png',
+    alt: 'project ggf-ui thumbnail image',
+  },
+  mingle: {
+    url: '/images/project-thumbnail-5.png',
+    alt: 'project mingle thumbnail image',
+  },
+  mingle_ui: {
+    url: '/images/project-thumbnail-6.png',
+    alt: 'project mingle-ui thumbnail image',
   },
 }
