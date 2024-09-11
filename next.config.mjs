68d70e78d4f097d2ea75dff6694ab751b74bc15e
iooi-alice <kimhee21c@hanmail.net>
2024-09-12 05:01:22 +0900

    feat: 초기 prettier 설정 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,6 +25,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
