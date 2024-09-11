73a213cb0aa9644185b3366111c571596322d7bb
iooi-alice <kimhee21c@hanmail.net>
2024-09-12 05:02:49 +0900

    feat: meta 데이터 상수화

---
+++ src/constants/meta.ts
@@ -0,0 +1,18 @@
+import { CLOUDFLARE_URL } from './images'
+
+export const META_DATA = {
+  title: 'Heee__soo Portfolio',
+  description:
+    '👋 I&#39;m designer & web developer focused on UX and design systems',
+  keywords: 'personal portfolio, product designer, publisher, front-end',
+  ogType: 'website',
+  ogDescription:
+    '👋 I&#39;m designer & web developer focused on UX and design systems',
+  ogImageWidth: '800',
+  ogImageHeight: '400',
+  ogSiteName: 'Heee__soo Portfolio',
+  siteUrl: 'https://soo-portfolio.vercel.app',
+  imageUrl: `${CLOUDFLARE_URL}/579719b4-944f-4783-bd0f-a55cf1c09400/width=800,height=400`,
+  imageAlt: 'Protfolio Thumbnail',
+  twitterImageUrl: `${CLOUDFLARE_URL}/93c8830f-78d9-421a-79c7-09aec4722c00/width=1200,height=630`,
+}
